c6e13b6f8c78e3b2ee1354d9ddb8eb0227e0e3d3
Raja-Simform <raja.jha@simformsolutions.com>
2025-05-08 12:18:21 +0530

    fix:add personalInfo,Skills,Constants file

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,98 +1,65 @@
-import { AboutStyled } from "./AboutStyled"
-import { motion, useInView } from "motion/react"
-import rajaImg from '../../assets/raja.png'
-import python from "../../assets/python.svg"
-import java from "../../assets/java.svg"
-import htmlIcon from "../../assets/html-icon.svg";
-import cssIcon from "../../assets/css-icon.svg";
-import jsIcon from "../../assets/js-icon.svg";
-import reactIcon from "../../assets/react-icon.svg";
-import typescriptIcon from "../../assets/typescript-icon.svg";
-import boostrapIcon from "../../assets/bootstrap-icon.svg";
-import { useRef } from "react"
-export default function About(){
- 
-  const aboutInfoRef = useRef(null);
+import { AboutStyled } from "./AboutStyled";
+import { motion, useInView } from "motion/react";
+import rajaImg from "../../assets/raja.png";
+
+import { useRef } from "react";
+import Skills from "../Skills/Skills";
+import PersonalInfo from "../PersonalInfo/PersonalInfo";
+export default function About() {
+  const aboutInfoRef = useRef<HTMLDivElement>(null);
   const educationRef = useRef(null);
   const experienceRef = useRef(null);
   const imageRef = useRef(null);
   const skillsTitleRef = useRef(null);
-  const skillContainerRef=useRef(null);
 
-  const isInfoInView = useInView(aboutInfoRef, { once: true, amount: 0.3 });
-  const isEducationInView = useInView(educationRef, { once: true, amount: 0.5 });
-  const isExperienceInView = useInView(experienceRef, { once: true, amount: 0.5 });
+  const isEducationInView = useInView(educationRef, {
+    once: true,
+    amount: 0.5,
+  });
+  const isExperienceInView = useInView(experienceRef, {
+    once: true,
+    amount: 0.5,
+  });
   const isImageInView = useInView(imageRef, { once: true, amount: 0.3 });
-  const isSkillsTitleInView = useInView(skillsTitleRef, { once: true, amount: 0.5 });
-  const isSkillsContainerInView = useInView(skillContainerRef, { once: true, amount: 0.3 });
+  const isSkillsTitleInView = useInView(skillsTitleRef, {
+    once: true,
+    amount: 0.5,
+  });
 
-  const skillsIcons = [
-    { src: python, alt: "Python", delay: 0.8 },
-    { src: java, alt: "Java", delay: 0.85 },
-    { src: jsIcon, alt: "JavaScript", delay: 0.9 },
-    { src: reactIcon, alt: "React", delay: 0.95 },
-    { src: typescriptIcon, alt: "TypeScript", delay: 1.0 },
-    { src: htmlIcon, alt: "HTML", delay: 1.2 },
-    { src: cssIcon, alt: "CSS", delay: 1.25 },
-    { src: boostrapIcon, alt: "Bootstrap", delay: 1.3 },
-  ];
-    return (
-    <AboutStyled id="about" >
+  return (
+    <AboutStyled id="about">
       <div className="about-info" ref={aboutInfoRef}>
-        <motion.p
+        <PersonalInfo aboutInfoRef={aboutInfoRef} />
+        <motion.div
+          className="education"
+          ref={educationRef}
           initial={{ opacity: 0, y: 50 }}
-          animate={isInfoInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.5 , delay: 0.1 }}
+          animate={
+            isEducationInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }
+          }
+          transition={{ duration: 0.5, delay: 0.2 }}
         >
-          About Me
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, y: 50 }}
-          animate={isInfoInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.5 , delay: 0.2}}
-        >
-         Hey there! I'm Raja, a passionate Software Developer with a strong foundation in full-stack web development.
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, y: 50 }}
-          animate={isInfoInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.5 , delay: 0.3}}
-        >
-        I create responsive web applications, build clean and scalable frontend systems, and enjoy working on collaborative real-time tools.
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, y: 50 }}
-          animate={isInfoInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.5 , delay: 0.4}}
-        >
-       I also love diving into system design, crafting efficient algorithms, and have hands-on experience with technologies like React.js, Node.js and Android (Java).
-        </motion.p>
-      <motion.div
-        className="education"
-        ref={educationRef}
-        initial={{opacity:0,y:50}}
-        animate={isEducationInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-        transition={{duration:0.5,delay:0.2}}
-      >
           <h3>Education</h3>
           <h5>Bachelor's of Engineering(BE) in Computer Engineering</h5>
           <p>L.D College of Engineering</p>
           <p>8.51 CGPA</p>
-      </motion.div>  
-      <motion.div
-        className="experience"
-        ref={experienceRef}
-        initial={{ opacity: 0, y: 50 }}
-        animate={isExperienceInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-      >
-       <h3>Experience:</h3>
-       <h4>Trainee Engineer</h4>
-       <p>Simform Solutions | Jan 2025 - Present</p>
-       <p>Ahmedabad, India</p>
-      </motion.div>
+        </motion.div>
+        <motion.div
+          className="experience"
+          ref={experienceRef}
+          initial={{ opacity: 0, y: 50 }}
+          animate={
+            isExperienceInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }
+          }
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          <h3>Experience:</h3>
+          <h4>Trainee Engineer</h4>
+          <p>Simform Solutions | Jan 2025 - Present</p>
+          <p>Ahmedabad, India</p>
+        </motion.div>
       </div>
-     
+
       <div className="about-img">
         <motion.div
           ref={imageRef}
@@ -102,28 +69,18 @@ export default function About(){
         >
           <img src={rajaImg} alt="profile-picture" />
         </motion.div>
-         <motion.h3
+        <motion.h3
           initial={{ opacity: 0, y: 50 }}
           ref={skillsTitleRef}
-          animate={isSkillsTitleInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+          animate={
+            isSkillsTitleInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }
+          }
           transition={{ duration: 0.5, delay: 0.2 }}
         >
           Here are my main skills:
-      </motion.h3>
-      <div className="hard-skills" ref={skillContainerRef}>
-          {skillsIcons.map((skill, index) => (
-             <div className="hability" key={index}>
-              <motion.img 
-                src={skill.src} 
-                alt={skill.alt}
-                initial={{ opacity: 0, y: 50 }}
-                animate={isSkillsContainerInView? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-                transition={{ duration: 0.5, delay: skill.delay }}
-              />
-            </div>
-          )
-          )}
-        </div>
+        </motion.h3>
+        <Skills />
       </div>
-    </AboutStyled>)
+    </AboutStyled>
+  );
 }
--- src/components/About/AboutStyled.tsx
+++ src/components/About/AboutStyled.tsx
@@ -9,9 +9,7 @@ export const AboutStyled = styled.section`
     flex-direction: row;
     align-items: center;
     min-height: 90vh;
-    
   }
-
 
   .about-info {
     flex: 3;
@@ -19,114 +17,109 @@ export const AboutStyled = styled.section`
     p:first-child {
       font-size: 2rem;
       font-weight: 700;
-      color:Green;
+      color: Green;
       margin-bottom: 1.5rem;
     }
-    
+
     p {
       margin-bottom: 1.5rem;
       font-size: 1.1rem;
       line-height: 1.6;
-      
     }
   }
-  
+
   .about-img {
     flex: 2;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    
+
     img {
       max-width: 100%;
       height: auto;
       border-radius: 12px;
-   
     }
-    
+
     @media (max-width: 991px) {
       margin-top: 3rem;
     }
   }
-  
-  .education, .experience {
+
+  .education,
+  .experience {
     margin: 2rem 0;
     padding: 1.5rem;
     background-color: #3f3d3d;
     border-radius: 8px;
-    
+
     h3 {
       font-size: 1.4rem;
-      color:green;
+      color: green;
       margin-bottom: 1rem;
     }
-    
+
     h4 {
       font-size: 1.2rem;
       margin-bottom: 0.5rem;
-   
     }
-    
+
     h5 {
       font-size: 1.1rem;
       margin-bottom: 0.5rem;
-    
     }
-    
+
     p {
       margin-bottom: 0.5rem;
       font-size: 1rem;
-  
     }
   }
-  
+
   h3 {
     text-align: center;
     margin: 2rem 0;
     font-size: 1.6rem;
     color: Green;
   }
-  
+
   .hard-skills {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 1rem;
     margin-bottom: 3rem;
-    
+
     @media (min-width: 576px) {
       grid-template-columns: repeat(4, 1fr);
       gap: 1.2rem;
     }
-    
+
     @media (min-width: 768px) {
       grid-template-columns: repeat(5, 1fr);
       gap: 1.5rem;
     }
-    
+
     @media (min-width: 992px) {
       grid-template-columns: repeat(6, 1fr);
     }
-    
-    .hability {
+
+    .ability {
       display: flex;
       justify-content: center;
       align-items: center;
       padding: 0.5;
       border-radius: 8px;
       transition: transform 0.3s ease;
-      
+
       &:hover {
         transform: translateY(-5px);
         box-shadow: 0 8px 20px rgba(0, 0, 0, 0.1);
       }
-      
+
       img {
         max-width: 100%;
         height: auto;
         max-height: 50px;
       }
     }
-
   }
 `;
