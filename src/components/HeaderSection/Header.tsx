7b7f26bb25ad98151afff67c27f6607b40713124
Raja-Simform <raja.jha@simformsolutions.com>
2025-05-13 18:50:47 +0530

    feat:add resume

--- src/components/HeaderSection/Header.tsx
+++ src/components/HeaderSection/Header.tsx
@@ -2,20 +2,27 @@ import { HeaderStyled } from "./HeaderStyled";
 import { LogoStyled } from "../Logo/LogoStyled";
 import { NavigationStyled } from "../Navigation/NavigationStyled";
 import { MobileMenuButtonStyled } from "../MobileMenuButton/MobileMenuButtonStyled";
+import Resume from '../../assets/Raja_Rsume _final.pdf'
+import { useState } from "react";
 export default function Header() {
+  const [open,setOpen]=useState(false);
+  function HandleToggle(){
+    setOpen(!open);
+  }
   return (
     <HeaderStyled>
       <LogoStyled>
         <p>{"< Raja Jha />"}</p>
       </LogoStyled>
-      <NavigationStyled className="navigation">
-        <a href="#home">Home</a>
-        <a href="#about">About Me</a>
-        <a href="#project">Projects</a>
-        <a href="#contact">Contact</a>
-        <button>Resume</button>
+      <NavigationStyled className={`navigation ${open ? 'open' : ''}`}>
+        <a href="#home" onClick={()=>setOpen(false)}>Home</a>
+        <a href="#about" onClick={()=>setOpen(false)}>About Me</a>
+        <a href="#projects" onClick={()=>setOpen(false)}>Projects</a>
+        <a href="#contact" onClick={()=>setOpen(false)}>Contact</a>
+        <a href={Resume} download onClick={()=>setOpen(false)} ><button>Resume</button></a>
       </NavigationStyled>
-      <MobileMenuButtonStyled aria-label="Toggle menu">
+      <MobileMenuButtonStyled aria-label="Toggle menu"
+       onClick={HandleToggle}>
         ☰
       </MobileMenuButtonStyled>
     </HeaderStyled>
--- src/components/HeaderSection/HeaderStyled.tsx
+++ src/components/HeaderSection/HeaderStyled.tsx
@@ -17,4 +17,3 @@ export const HeaderStyled = styled.section`
 
 
 
-
